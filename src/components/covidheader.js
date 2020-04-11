49f444e13e00182b326df206ab5cfe6dba63bf0d
Zishan <zishanmohammed02@gmail.com>
2020-04-12 01:40:18 +0530

    logo change

--- src/components/covidheader.js
+++ src/components/covidheader.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import styles from './mystyle.module.css';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from './logo.png';
+
 
 class CovidHeader extends Component{
 
-render(){ return<h1> Welcome to TrackCovid-19</h1>}
+render(){return (
+      <div id="logo"><img
+        src={logo}
+        width="10%"
+        height="10%"
+        className="d-inline-block align-top"
+        alt="TrackCovid-19"
+      /></div>
+    
+  )}
 
 
 }
